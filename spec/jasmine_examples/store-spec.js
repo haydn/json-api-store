6684bf96a6e9e56fe3736d92f7a4176b798071e9
Haydn Ewers <haydn@particlesystem.com>
2015-08-11 18:41:39 +0930

    Add basic find, add and push methods.

--- spec/jasmine_examples/store-spec.js
+++ spec/jasmine_examples/store-spec.js
@@ -1,19 +1,112 @@
+var Store = require("../../src/store");
+
 describe("Store", function() {
 
-  var Store, store;
+  var store;
 
   beforeEach(function() {
-    Store = require("../../src/store");
-    // Store.ajax = fakejax;
+    Store.types = {};
     store = new Store();
   });
 
-  it("should have a push method", function () {
-    expect().toBeDefined();
+  describe("find", function () {
+
+    it("must return an object with type and id fields when an id is provided", function () {
+      Store.types["products"] = {};
+      expect(store.find("products", "23").type).toBe("products");
+      expect(store.find("products", "74").id).toBe("74");
+    });
+
+    it("must return the same object if called with the same arguments", function () {
+      Store.types["products"] = {};
+      expect(store.find("products", "23")).toBe(store.find("products", "23"));
+    });
+
+    it("must return an array of objects when no id is provided", function () {
+      Store.types["products"] = {};
+      var a = store.find("products", "1");
+      expect(store.find("products")).toContain(a);
+    });
+
+    it("must throw an error when trying to find an unknown resource type", function () {
+      expect(function () {
+        store.find("products");
+      }).toThrowError(TypeError, "Missing type 'products'");
+      expect(function () {
+        store.find("products", "1");
+      }).toThrowError(TypeError, "Missing type 'products'");
+    });
+
+    it("must throw an error when called without arguments", function () {
+      expect(function () {
+        store.find();
+      }).toThrowError(TypeError, "You must provide a type");
+    });
+
   });
 
-  it("should have a push method", function () {
-    expect(store.push).toBeDefined();
+  describe("add", function () {
+
+    it("must add a resource to the store", function () {
+      Store.types["products"] = {};
+      store.add({
+        "type": "products",
+        "id": "44"
+      });
+      expect(store.find("products").length).toBe(1);
+      expect(store.find("products")[0].id).toBe("44");
+    });
+
+    it("must throw an error if the data doesn't have a type and id", function () {
+      expect(function () {
+        store.add({});
+      }).toThrowError(TypeError, "The data must have a type and id");
+    });
+
+    it("must throw an error when called without arguments", function () {
+      expect(function () {
+        store.add();
+      }).toThrowError(TypeError, "You must provide data to add");
+    });
+
+  });
+
+  describe("push", function () {
+
+    it("must add a single resource to the store", function () {
+      Store.types["products"] = {};
+      store.push({
+        "data": {
+          "type": "products",
+          "id": "34"
+        }
+      });
+      expect(store.find("products").length).toBe(1);
+      expect(store.find("products")[0].id).toBe("34");
+    });
+
+    it("must add a collection of resources to the store", function () {
+      Store.types["products"] = {};
+      store.push({
+        "data": [
+          {
+            "type": "products",
+            "id": "34"
+          },
+          {
+            "type": "products",
+            "id": "74"
+          }
+        ]
+      });
+      expect(store.find("products").length).toBe(2);
+      var ids = store.find("products").map(function (x) {
+        return x.id;
+      });
+      expect(ids).toContain("34");
+      expect(ids).toContain("74");
+    });
+
   });
 
 });
--- src/store.js
+++ src/store.js
@@ -1,7 +1,49 @@
 export default class Store {
 
-  push() {
-    return;
+  constructor() {
+    this._data = {};
+  }
+
+  find(type, id) {
+    if (type) {
+      if (Store.types[type]) {
+        this._data[type] = this._data[type] || {};
+        if (id) {
+          return this._data[type][id] = this._data[type][id] || {
+            "type": type,
+            "id": id
+          };
+        } else {
+          return Object.keys(this._data[type]).map(x => this._data[type][x]);
+        }
+      } else {
+        throw new TypeError(`Missing type '${type}'`);
+      }
+    } else {
+      throw new TypeError(`You must provide a type`);
+    }
+  }
+
+  push(root) {
+    if (root.data.constructor === Array) {
+      return root.data.map(x => this.add(x));
+    } else {
+      return this.add(root.data);
+    }
+  }
+
+  add(data) {
+    if (data) {
+      if (data.type && data.id) {
+        return this.find(data.type, data.id);
+      } else {
+        throw new TypeError(`The data must have a type and id`);
+      }
+    } else {
+      throw new TypeError(`You must provide data to add`);
+    }
   }
 
 }
+
+Store.types = {};
