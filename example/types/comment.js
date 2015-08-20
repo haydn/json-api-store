d8608600add69c044b70515b87a3e1ba200eb6df
Haydn Ewers <haydn@particlesystem.com>
2015-08-20 10:53:33 +0930

    Add support for pseudonyms.

--- example/types/comment.js
+++ example/types/comment.js
@@ -1,5 +1,4 @@
 var Comment = {
   body: Store.attr(),
-  // FIXME: There shouldn't be a need to set the inverse relationship here.
-  product: Store.hasOne({ inverse: "comments" })
+  product: Store.hasOne()
 };
--- spec/store/core/define-spec.js
+++ spec/store/core/define-spec.js
@@ -8,4 +8,28 @@ describe("define", function() {
     store = new Store();
   });
 
+  it("must accept pseudonyms", function () {
+    store.define([ "comments", "comment" ], {
+      product: Store.hasOne()
+    });
+    store.define([ "products", "product" ], {
+      comments: Store.hasMany()
+    });
+    expect(store.find("comment", "67")).toBe(store.find("comments", "67"));
+    expect(store.find("product", "8")).toBe(store.find("products", "8"));
+    store.add({
+      "type": "comment",
+      "id": "1",
+      "relationships": {
+        "product": {
+          "data": {
+            "type": "products",
+            "id": "1"
+          }
+        }
+      }
+    });
+    expect(store.find("product", "1").comments[0]).toBe(store.find("comments", "1"));
+  });
+
 });
